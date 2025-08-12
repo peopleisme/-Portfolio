673b617540f5b1298d551923c86deba846b61c81
peopleisme <socko.piotr@o2.pl>
2025-08-12 23:03:45 +0200

    Name animation and profile pic

---
+++ script/script.js
@@ -0,0 +1,27 @@
+el_name = document.getElementById("name");
+el_surName = document.getElementById("surName");
+
+
+window.addEventListener("scroll", () => {
+    WscrollY = window.scrollY;
+    //console.log("Scroll Y:", window.scrollY);
+
+    if (WscrollY > window.innerHeight * 0 && WscrollY < window.innerHeight * 0.3) {
+        el_name.style.left = "calc(10% - " + 10 * WscrollY / (window.innerHeight * 0.3) + "%)";
+        el_name.style.fontSize = "calc(15vh - " + 12 * WscrollY / (window.innerHeight * 0.3) + "vh)";
+        el_surName.style.top = "calc(45% - " + 45 * WscrollY / (window.innerHeight * 0.3) + "%)";
+        console.log(WscrollY / (window.innerHeight * 0.3));
+        
+    }
+    if (WscrollY / (window.innerHeight * 0.3) > 1) {
+        console.log(WscrollY / (window.innerHeight * 0.3));
+        el_name.style.position = "fixed";
+        el_name.style.top = "0";
+        el_name.style.left = "0";
+        el_name.style.fontSize = "3vh";
+
+    } else {
+        el_name.style.position = "absolute";
+        el_name.style.top = "30%";
+    }
+});
