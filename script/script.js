b87e621d0043084ad48f759a2f9d1a9fc29d2597
Piotr Socko <socko.piotr@o2.pl>
2025-08-20 19:47:29 +0200

    About me

--- script/script.js
+++ script/script.js
@@ -9,15 +9,12 @@ window.addEventListener("scroll", () => {
     if (WscrollY > window.innerHeight * 0 && WscrollY < window.innerHeight * 0.3) {
         el_name.style.left = "calc(10% - " + 10 * WscrollY / (window.innerHeight * 0.3) + "%)";
         el_name.style.fontSize = "calc(15vh - " + 12 * WscrollY / (window.innerHeight * 0.3) + "vh)";
-        el_surName.style.top = "calc(45% - " + 45 * WscrollY / (window.innerHeight * 0.3) + "%)";
+        el_surName.style.top = "calc(45% - " + 60 * WscrollY / (window.innerHeight * 0.3) + "%)";
         console.log(WscrollY / (window.innerHeight * 0.3));
 
     }
     if (WscrollY / (window.innerHeight * 0.3) > 1) {
         console.log(WscrollY / (window.innerHeight * 0.3));
-
-
-
         el_name.style.position = "fixed";
         el_name.style.top = "0";
         el_name.style.left = "0";
