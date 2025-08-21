aa8b97f202d3974d419dfeaca28ce697a3cedc33
peopleisme <socko.piotr@o2.pl>
2025-08-21 21:40:17 +0200

    media query scalability

--- script/script.js
+++ script/script.js
@@ -5,23 +5,41 @@ el_surName = document.getElementById("surName");
 window.addEventListener("scroll", () => {
     WscrollY = window.scrollY;
     //console.log("Scroll Y:", window.scrollY);
+    if (window.innerWidth > 800) {
+        if (WscrollY > window.innerHeight * 0 && WscrollY < window.innerHeight * 0.3) {
+            el_name.style.left = "calc(10% - " + 10 * WscrollY / (window.innerHeight * 0.3) + "%)";
+            el_name.style.fontSize = "calc(15vh - " + 12 * WscrollY / (window.innerHeight * 0.3) + "vh)";
+            el_surName.style.top = "calc(45% - " + 60 * WscrollY / (window.innerHeight * 0.3) + "%)";
+            console.log(WscrollY / (window.innerHeight * 0.3));
+        }
+        if (WscrollY / (window.innerHeight * 0.3) > 1) {
+            console.log(WscrollY / (window.innerHeight * 0.3));
+            el_name.style.position = "fixed";
+            el_name.style.top = "0";
+            el_name.style.left = "0";
+            el_name.style.fontSize = "3vh";
 
-    if (WscrollY > window.innerHeight * 0 && WscrollY < window.innerHeight * 0.3) {
-        el_name.style.left = "calc(10% - " + 10 * WscrollY / (window.innerHeight * 0.3) + "%)";
-        el_name.style.fontSize = "calc(15vh - " + 12 * WscrollY / (window.innerHeight * 0.3) + "vh)";
-        el_surName.style.top = "calc(45% - " + 60 * WscrollY / (window.innerHeight * 0.3) + "%)";
-        console.log(WscrollY / (window.innerHeight * 0.3));
-
-    }
-    if (WscrollY / (window.innerHeight * 0.3) > 1) {
-        console.log(WscrollY / (window.innerHeight * 0.3));
-        el_name.style.position = "fixed";
-        el_name.style.top = "0";
-        el_name.style.left = "0";
-        el_name.style.fontSize = "3vh";
-
+        } else {
+            el_name.style.position = "absolute";
+            el_name.style.top = "30%";
+        }
     } else {
-        el_name.style.position = "absolute";
-        el_name.style.top = "30%";
+        if (WscrollY > window.innerHeight * 0 && WscrollY < window.innerHeight * 0.3) {
+            el_name.style.left = "calc(10% - " + 10 * WscrollY / (window.innerHeight * 0.3) + "%)";
+            el_name.style.fontSize = "calc(8vh - " + 5 * WscrollY / (window.innerHeight * 0.3) + "vh)";
+            el_surName.style.top = "calc(38% - " + 60 * WscrollY / (window.innerHeight * 0.3) + "%)";
+            console.log("WscrollY", WscrollY);
+            console.log("document.body.scrollTop:", document.body.scrollTop);
+        } else if (WscrollY / (window.innerHeight * 0.3) > 1) {
+            console.log(WscrollY / (window.innerHeight * 0.3));
+            el_name.style.position = "fixed";
+            el_name.style.top = "0";
+            el_name.style.left = "0";
+            el_name.style.fontSize = "3vh";
+
+        } else if (WscrollY < 10) {
+            el_name.style.position = "absolute";
+            el_name.style.top = "30%";
+        }
     }
 });
